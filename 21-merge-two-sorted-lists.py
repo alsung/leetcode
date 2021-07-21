36a7009aa8068fffe9e407db68a5635ae9ffd4aa
Alex Sung <alexsung98@gmail.com>
2021-07-20 23:41:26 -0700

    create 21-merge-two-sorted-lists [redo x2]

---
+++ 21-merge-two-sorted-lists.py
@@ -0,0 +1,12 @@
+# Leetcode Question 21
+
+# Merge two sorted linked lists and return it as a sorted list. 
+# The list should be made by splicing together the nodes of the 
+# first two lists. 
+
+# Example 1: 
+# input: l1 = [1,2,4], l2 = [1,3,4]
+# output: [1,1,2,3,4,4]
+
+# input: l1 = [], l2 = []
+# output: []
