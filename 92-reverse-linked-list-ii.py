d62c34d88397759914397e7463e0be1f3f584b0e
Alex Sung <alexsung98@gmail.com>
2021-07-27 14:41:50 -0700

    leetcode q92 completed
    Reverse Linked List II: Using two int pointers left and right, inclusively reverses all nodes between left and right pointer, returns final reverse linked list.

--- 92-reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii.py
@@ -11,3 +11,36 @@
 # input: head = [5], left = 1, right = 1
 # output: [5]
 
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def reverseBetween(self, head, left, right):
+        """
+        :type head: ListNode
+        :type left: int
+        :type right: int
+        :rtype: ListNode
+        """
+        if not head or left == right:
+            return head
+        
+        # use dummy to ensure there is always available pre node if left = 1
+        dummy = pre = ListNode(None) 
+        dummy.next = head
+        for i in range(left-1):
+            pre = pre.next # puts pre ptr into position left - 1
+        cur = pre.next # cur ptr at position left
+
+        for i in range(right-left):
+            tmp = pre.next
+            pre.next = cur.next
+            cur.next = cur.next.next
+            pre.next.next = tmp
+        return dummy.next
+
+
+
+        
