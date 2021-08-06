5c6c8f22ae6129b36ba85fdd8b717b8cb4591184
Alex Sung <alexsung98@gmail.com>
2021-08-05 20:32:07 -0700

    fix off by 1 error

--- 198-house-robber.py
+++ 198-house-robber.py
@@ -275,7 +275,7 @@ class Solution3(object):
         rob_next = nums[N - 1]
 
         # DP table calculations
-        for i in range(N - 1, -1, -1):
+        for i in range(N - 2, -1, -1):
 
             # Same as recursive solution
             current = max(rob_next, rob_next_plus_one + nums[i])
