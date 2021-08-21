bd0a0030ccd7525d6eaae39642d1dd0abe03a5de
Alex Sung <alexsung98@gmail.com>
2021-08-21 02:03:43 -0700

    completed lc 322 coin change dp problem [redo x2]
    dp solution building a 1d dp array to store values. time O(amount * len(coins)) and space O(amount)

---
+++ 322-coin-change.py
@@ -0,0 +1,52 @@
+# Leetcode Question 322 [Medium]
+# Coin Change
+
+# You are given an integer array coins representing coins of different 
+# denominations and an integer amount representing a total amount of money. 
+
+# Return the fewest number of coins that you need to make up that amount. If 
+# that amount of money cannot be made up by any combination of coins, 
+# return -1. 
+
+# You may assume that you have an infinite number of each kind of coin. 
+
+# Example 1:
+# Input: coins = [1,2,5], amount = 11
+# Output: 3
+# Explanation: 11 = 5 + 5 + 1
+
+# Example 2: 
+# Input: coins = [2], amount = 3
+# Output: -1
+
+# Example 3: 
+# Input: coins = [1], amount = 0
+# Output: 0
+
+# Example 4: 
+# Input: coins = [1], amount = 1
+# Output: 1
+
+# Example 5: 
+# Input: coins = [1], amount = 2
+# Output: 2
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        dp = [amount + 1] * (amount + 1) # 0,...,amount
+        dp[0] = 0
+
+        for a in range(1, amount + 1):
+            for c in coins:
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - c])
+                    
+        return dp[amount] if dp[amount] != amount + 1 else -1
+    
+# Time Complexity: O(amount * len(coins))
+# Space Complexity: O(amount)
