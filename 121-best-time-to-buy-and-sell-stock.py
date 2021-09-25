e94687d30789d57b963fbb9d3f17def603525879
Alex Sung <alexsung98@gmail.com>
2021-09-24 17:15:15 -0700

    completed lc 121 easy DP array problem

---
+++ 121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,59 @@
+# Leetcode Question 121 [Easy]
+# Best Time to Buy and Sell Stock
+
+"""
+You are given an array prices where prices[i] is the price of a given stock 
+on the i-th day. 
+
+You want to maximize your profit by choosing a single day to buy one stock and 
+choosing a different day in the future to sell that stock. 
+
+Return the maximum profit you can achieve from this transaction. If you cannot
+achieve any profit, return 0. 
+
+Example 1:
+Input: prices = [7, 1, 5, 3, 6, 4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 
+6-1 = 5. Note that buying on day 2 and selling on day 1 is not allowed because
+you must buy before you sell. 
+
+Example 2: 
+Input: prices = [7, 6, 4, 3, 1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0. 
+
+Constraints: 
+    1 <= prices.length <= 10^5
+    0 <= prices[i] <= 10^4
+"""
+
+# One Pass Approach
+# Say the given array is: [7,1,5,3,6,4]
+# The points of interest are the peaks and valleys in the given graph. We need 
+# to find the largest peak following the smallest valley. We can maintain two 
+# variables - min_price and max_profit corresponding to the smallest valley 
+# and maximum profit (maximum different between selling price and min_price) 
+# obtained so far respectively. 
+
+import math
+
+class Solution: 
+    def maxProfit(self, prices): 
+        min_price = math.inf
+        max_profit = 0
+        n = len(prices) 
+
+        for i in range(n):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            elif prices[i] - min_price > max_profit:
+                max_profit = prices[i] - min_price
+        
+        return max_profit
+
+test = Solution
+print(test.maxProfit(test, [7,1,5,3,6,4]))
+
+# Time Complexity: O(N), One pass
+# Space Complexity: O(1)
