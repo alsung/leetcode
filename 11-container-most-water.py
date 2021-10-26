52155898ad3664a5c27e18ff3db70a8e62c46344
Alex Sung <alexsung98@gmail.com>
2021-10-26 11:17:00 -0700

    LC 11 container with most water completed x2
    neetcode approach using two pointers

--- 11-container-most-water.py
+++ 11-container-most-water.py
@@ -37,3 +37,64 @@ class Solution(object):
 
 # Time Complexity: O(N)
 # Space Complexity: O(1)
+
+###############################################################################
+# Neetcode Approach: https://www.youtube.com/watch?v=UuiTKBwPgAo&list=PLot-Xpze53ldVwtstag2TL4HQhAnC8ATf&index=10
+
+"""
+Given n non-negative integers a1, a2, ..., an, where each represents a point at
+coordinate (i, a[i]). n vertical lines are drawn such that the two endpoints 
+of the line i is at (i, a[i]) and (i, 0). Find two lines, which, together with
+the x-axis forms a container, such that the container contains the most water. 
+
+Notice that you may not slant the container. 
+
+Example 1: 
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array 
+[1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the 
+container can contain is 49. 
+"""
+
+class Solution1:
+    def maxArea(self, height):
+        # BRUTE FORCE
+        res = 0
+
+        for l in range(len(height)):
+            for r in range(l+1, len(height)):
+                area = (r - l) * min(height[l], height[r])
+                res = max(res, area)
+
+        return res
+
+# Time: O(N^2)
+
+# Exceeds Time Limit
+
+"""
+Try two pointer technique
+- place left pointer at first element
+- place right pointer at last element to maximize area
+"""
+
+class Solution2: 
+    def maxArea(self, height):
+        res = 0
+        l, r = 0, len(height) - 1
+
+        while l < r:
+            area = (r - l) * min(height[l], height[r])
+            res = max(res, area)
+
+            if height[l] < height[r]: # left height shorter than right 
+                # shift left pointer to right, search for larger height
+                l += 1
+            else:
+                l += 1
+
+        return res
+
+# Time: O(N)
+# Space: O(1)
